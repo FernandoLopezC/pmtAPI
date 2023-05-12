b4e4d93a6f3365a06fb24e32ab3642fe2d16de88
FernandoLopezC <lopez.castrof28@gmail.com>
2023-05-12 20:02:26 +0100

    API created

---
+++ MixMateDB.sql
@@ -0,0 +1,118 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.2
+-- Dumped by pg_dump version 14.2
+
+-- Started on 2023-05-12 19:59:54
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 210 (class 1259 OID 32780)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name character varying(60) NOT NULL,
+    email character varying(60) NOT NULL,
+    password character varying(120),
+    date_of_birth date,
+    premium boolean DEFAULT false
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 209 (class 1259 OID 32779)
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3316 (class 0 OID 0)
+-- Dependencies: 209
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- TOC entry 3164 (class 2604 OID 32783)
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- TOC entry 3310 (class 0 OID 32780)
+-- Dependencies: 210
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, email, password, date_of_birth, premium) FROM stdin;
+1	admin	admin@admin.com	pbkdf2:sha256:260000$q7ngsxr9Xn2jv5s4$8500112eb1935f65a1ae577e12db32290cb8bc6a87708ad0697d2d31269d3990	2001-08-28	f
+\.
+
+
+--
+-- TOC entry 3317 (class 0 OID 0)
+-- Dependencies: 209
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+
+
+--
+-- TOC entry 3167 (class 2606 OID 32787)
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- TOC entry 3169 (class 2606 OID 32785)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+-- Completed on 2023-05-12 19:59:54
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ app/routes.py
@@ -0,0 +1,161 @@
+from app import app, login, admin, db
+from flask import render_template, flash, redirect, url_for, request, session, \
+    g, current_app, abort, jsonify
+from flask_login import current_user, login_user, logout_user
+from flask_login import login_required
+from app.forms import *
+from app.models import users_tbl, DbUser
+import sys
+import os
+from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
+from shutil import copyfile
+
+
+@login.user_loader
+def load_user(id):
+    user = users_tbl.query.get(int(id))
+    if user:
+        return DbUser(user)
+    else:
+        return None
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('main.html', title='home', page='home')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # checks if the user is authenticated , if they are it redirects the user
+    # to the index page
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+
+    # checks the form validation, if the user does not validate it returns
+    # them to the login page and tells them invalid password or username
+    if form.validate_on_submit():
+        check_user = users_tbl.query.filter_by(email=form.email.data).first()
+        user = DbUser(check_user)
+
+        if check_user is None or not user.check_password(form.password.data):
+            app.logger.warning('User fail to validate')
+            flash('Invalid username or password')
+
+            return redirect(url_for('login'))
+        if not user.active:
+
+            db.session.commit()
+
+        # logs in the user sets them as active and directs them to the page
+        # they tried to access if not it directs them to the index page
+        if form.remember_me.data:
+            login_user(user, remember=True)
+        else:
+            login_user(user, remember=False)
+
+        next_page = request.args.get('next')
+
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+
+    return render_template('login.html', title='Sign In',
+                           form=form, page='login')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    """
+    Logs user out
+    """
+    logout_user()
+    flash('You have logged out')
+
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = users_tbl(name=form.name.data, email=form.email.data,date_of_birth=form.date_of_birth.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/check_premium/<user>', methods=['GET','POST'])
+@login_required
+def check_premium(user):
+    """
+    fetches the details of an user to fill in form data when updating that user
+    """
+    if user:
+        cur_user = users_tbl.query.filter_by(id=user).first()
+
+        UserObj = {}
+
+        if cur_user.premium:
+            UserObj["premium"] = True
+        else:
+            UserObj["premium"] = False
+
+        return jsonify(UserObj)
+    else:
+        return "None"
+
+
+@app.route('/login_user/<user>/<pw>', methods=['GET', 'POST'])
+@login_required
+def check_premium(user, pw):
+    """
+    fetches the details of an user to fill in form data when updating that user
+    """
+    if user:
+        cur_user = users_tbl.query.filter_by(email=user).first()
+
+        UserObj = {}
+
+        user = DbUser(cur_user)
+        if user.check_password(pw):
+            UserObj["login"] = True
+        else:
+            UserObj["login"] = False
+
+        return jsonify(UserObj)
+    else:
+        return "None"
+
+
+@app.route("/update_details", methods=['Get', 'POST'])
+@login_required
+def update_details():
+
+    form = UpdateUser()
+    if current_user.is_authenticated:
+        form.user_name.data = current_user._user.name
+        form.user_email.data = current_user._user.email
+        if not form.user_date_of_birth.data:
+            form.user_date_of_birth.data = current_user._user.date_of_birth
+        if form.validate_on_submit():
+            cur_user = users_tbl.query.filter_by(id=current_user._user.id).first()
+            cur_user.name = form.user_name.data
+            if form.user_new_email.data:
+                cur_user.email = form.user_new_email.data
+            if form.user_new_name.data:
+                cur_user.name = form.user_new_name.data
+
+            cur_user.date_of_birth = form.user_date_of_birth.data
+            db.session.commit()
+            flash("User details have been updated")
+            return redirect(url_for("index"))
+
+        return render_template('edit_details.html', form=form, page='Update details')
