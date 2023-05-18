990b92b18555ba97b04e45f037cdb545e97ae4db
FernandoLopezC <lopez.castrof28@gmail.com>
2023-05-18 11:44:30 +0100

    Updated dbackup and spec for python env

--- MixMateDB.sql
+++ MixMateDB.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.2
--- Dumped by pg_dump version 14.2
+-- Dumped from database version 14.4
+-- Dumped by pg_dump version 14.4
 
--- Started on 2023-05-12 19:59:54
+-- Started on 2023-05-18 11:42:55
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,7 +23,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 210 (class 1259 OID 32780)
+-- TOC entry 209 (class 1259 OID 24612)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -39,7 +39,7 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
--- TOC entry 209 (class 1259 OID 32779)
+-- TOC entry 210 (class 1259 OID 24616)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -56,7 +56,7 @@ ALTER TABLE public.users_id_seq OWNER TO postgres;
 
 --
 -- TOC entry 3316 (class 0 OID 0)
--- Dependencies: 209
+-- Dependencies: 210
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -64,7 +64,7 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 3164 (class 2604 OID 32783)
+-- TOC entry 3165 (class 2604 OID 24617)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -72,27 +72,28 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 3310 (class 0 OID 32780)
--- Dependencies: 210
+-- TOC entry 3309 (class 0 OID 24612)
+-- Dependencies: 209
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.users (id, name, email, password, date_of_birth, premium) FROM stdin;
-1	admin	admin@admin.com	pbkdf2:sha256:260000$q7ngsxr9Xn2jv5s4$8500112eb1935f65a1ae577e12db32290cb8bc6a87708ad0697d2d31269d3990	2001-08-28	f
+COPY public.users (id, name, email, password, premium) FROM stdin;
+2	admin	admin@admin.com	pbkdf2:sha256:260000$xjMshjLWQpv55NwZ$fba2c74aba40efde6a6c145e0d4a762cae6d97f72c7cd560d143b6f516c5e12b	f
+3	bjh	hhh	pbkdf2:sha256:260000$dcPfvjww4a6hC7TV$ffcc435df976f90bf91ebd248c0154bb39f4276477bf9c3b9de788a54abb6395	f
 \.
 
 
 --
 -- TOC entry 3317 (class 0 OID 0)
--- Dependencies: 209
+-- Dependencies: 210
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+SELECT pg_catalog.setval('public.users_id_seq', 4, true);
 
 
 --
--- TOC entry 3167 (class 2606 OID 32787)
+-- TOC entry 3167 (class 2606 OID 24619)
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -101,7 +102,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 3169 (class 2606 OID 32785)
+-- TOC entry 3169 (class 2606 OID 24621)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -109,7 +110,7 @@ ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
--- Completed on 2023-05-12 19:59:54
+-- Completed on 2023-05-18 11:42:56
 
 --
 -- PostgreSQL database dump complete
